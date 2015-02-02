3127fd323a307d5eaba39cb1cc8f0e8cdeef6378
Miguel Briales Romanillos <brialesromanillos@gmail.com>
2015-02-02 19:32:40 +0100

    Exercise Done

---
+++ passwd.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Miguel Briales Romanillos
+# 05941778-G
+
+# Opens and reads file
+File_Users = open ("/etc/passwd") # No tiene "modo" dado que por defecto es "Read"
+User_Names = File_Users.readlines()
+
+# Searches user names
+User_List = {}
+for Users in User_Names:
+    if Users.find('#'): # For delete comments
+    # Looks for user
+        End_User = Users.find(':*:')
+        User = Users[:End_User]
+    # Looks for shell
+        Users_Reverted = Users[::-1]
+        Start_Shell = Users_Reverted.find(':')
+        Shell = Users_Reverted[:Start_Shell]
+        Shell = Shell[::-1]
+
+# Writes the dictionary
+        User_List[User] = Shell
+
+# Prints number of users
+print ('Hay ' + str(len(User_List)) + ' usuarios\n')
+
+# Prints "root" and "imaginary" user
+try:
+    print User_List['root']
+    print User_List['imaginario']
+# In case an user doesn't exist
+except: 
+    print 'ERROR! El usuario introducido no existe\n'
